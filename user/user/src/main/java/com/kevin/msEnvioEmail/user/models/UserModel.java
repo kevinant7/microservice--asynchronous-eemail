813e9687039053d06434408a0423411fa9fa21b5
kevin <kevinant7@gmail.com>
2025-04-12 16:34:29 -0300

    reimport user

--- user/user/src/main/java/com/kevin/msEnvioEmail/user/models/UserModel.java
+++ user/user/src/main/java/com/kevin/msEnvioEmail/user/models/UserModel.java
@@ -1,10 +1,6 @@
 package com.kevin.msEnvioEmail.user.models;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Table;
-import org.springframework.data.annotation.Id;
+import jakarta.persistence.*;
 
 import java.io.Serializable;
 import java.util.UUID;
@@ -15,7 +11,7 @@ public class UserModel implements Serializable {
     private static final long serialVersionUID = 1L;
 
     @Id
-    @GeneratedValue(strategy= GenerationType.AUTO)
+    @GeneratedValue(strategy = GenerationType.AUTO)
     private UUID userId;
     private String name;
     private String email;
